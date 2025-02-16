c982d40a5fbff96ffbb8a5b7626a67eb15af197b
John Connell <johnconnellcs@gmail.com>
2025-02-15 19:38:02 -0800

    Set up firebase auth

--- src/app/lib/Firebase.js
+++ src/app/lib/Firebase.js
@@ -5,8 +5,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  //...
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_DOMAIN_KEY,
+    projectId: "duckyoumean-6a6c1",
+    storageBucket: "duckyoumean-6a6c1.firebasestorage.app",
+    messagingSenderId: "760213869834",
+    appId: "1:760213869834:web:830452a0dbb177567416f3"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
